d5ec1faad97f4c77750aa4b31e7f55e0982c2434
和ノ助（わのすけ） <wanosuke.t@gmail.com>
2025-02-02 17:06:36 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,80 @@
-console.log($);
-// ドロワーメニューの開閉
-jQuery("#js-link-menu-drawer").on("click", function () {
-  jQuery(this).next().slideToggle();
+$(document).ready(function () {
+  // ドロワーメニューの開閉
+  const menuToggle = jQuery("#js-link-menu-drawer");
+  const menuDrawer = jQuery("#js-menu-drawer");
+  const header = jQuery(".header");
+
+  // 📌 共通化した関数（メニューの位置を更新）
+  function updateMenuPosition() {
+    const rect = header[0].getBoundingClientRect();
+    menuDrawer.css({
+      top: rect.bottom + "px",
+      left:
+        menuToggle.offset().left -
+        (menuDrawer.width() - menuToggle.width()) / 2 +
+        "px",
+    });
+  }
+
+  menuToggle.on("click", function (event) {
+    event.stopPropagation(); // 他のクリックイベントを防ぐ
+
+    updateMenuPosition(); // メニューの位置を設定
+    menuDrawer.slideToggle();
+  });
+
+  // 外部クリックでメニューを閉じる
+  $(document).on("click", function (event) {
+    if (
+      !menuDrawer.is(event.target) &&
+      !menuToggle.is(event.target) &&
+      !menuDrawer.has(event.target).length
+    ) {
+      menuDrawer.hide();
+    }
+  });
+
+  // 📌 ウィンドウサイズ変更時に位置を更新
+  $(window).on("resize", function () {
+    console.log("リサイズ");
+    if (menuDrawer.is(":visible")) {
+      updateMenuPosition();
+    }
+  });
+
+  // ページトップへ戻るボタン
+  jQuery(".js-page-top").on("click", function () {
+    jQuery("html, body").animate({ scrollTop: 0 }, 500);
+  });
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 80) {
+      jQuery(".js-page-top").fadeIn(300);
+    } else {
+      jQuery(".js-page-top").fadeOut(300);
+    }
+  });
+
+  // モーダルで画像の拡大表示
+  // 変数に要素を入れる
+  const open = $(".js-menu-img");
+  const container = $(".modal-container");
+
+  //開くボタンをクリックしたらモーダルを表示する
+  open.on("click", function () {
+    const imgFile = $(this).attr("src");
+    $(".modal-container img").attr("src", imgFile);
+    container.addClass("active");
+    $("body").addClass("is-fixed");
+
+    return false;
+  });
+
+  //モーダルの外側をクリックしたらモーダルを閉じる
+  $(document).on("click", function (e) {
+    if (!$(e.target).closest(".modal-body").length) {
+      container.removeClass("active");
+      $("body").removeClass("is-fixed");
+    }
+  });
 });
